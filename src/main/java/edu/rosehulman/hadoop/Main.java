980cb114da72aaf972448861a3fb34857996a9bd
Christopher Keinsley <keinsleychris@gmail.com>
2017-10-24 20:21:21 -0400

    Heyo

--- src/main/java/edu/rosehulman/hadoop/Main.java
+++ src/main/java/edu/rosehulman/hadoop/Main.java
@@ -1,6 +1,7 @@
 package edu.rosehulman.hadoop;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import org.apache.hadoop.conf.Configuration;
@@ -9,17 +10,17 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
 public class Main {
-	private TableName table1 = TableName.valueOf("Table1");
-	private String family1 = "Family1";
-	private String family2 = "Family2";
+
+	private Admin admin;
 
 	public void run() {
 		attemptConnection();
+		printTables();
 		Scanner in = new Scanner(System.in);
 		while (true) {
 			System.out.print("PlayBall > ");
@@ -39,32 +40,44 @@ public class Main {
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.out.println("IOException when connecting");
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.err.println("I CAUGHT AN ERROR. FIX THIS LATER");
-		} finally {
-			System.out.println("------------------FINALLY---------------");
 		}
 	}
 
 	private void connect() throws IOException {
 		System.out.println("Constructing Config");
 		Configuration config = HBaseConfiguration.create();
-		System.out.println("Adding Resource");
 		config.addResource("hbase-site.xml");
 		config.set("zookeeper.znode.parent", "/hbase-unsecure");
+		
 		System.out.println("Creating Connection");
 		Connection conn = ConnectionFactory.createConnection(config);
-		System.out.println("Getting Admin");
-		Admin admin = conn.getAdmin();
-		System.out.println("Tables Should be Soon");
-		TableName[] tables = admin.listTableNames();
-		System.out.println("Table Names: " + tables);
+		admin = conn.getAdmin();
+	}
+
+	private void printTables() {
+		try {
+			TableName[] tables = getTables();
+			System.out.println("Table Names: " + Arrays.toString(tables));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private TableName[] getTables() throws IOException {
+		System.out.println("Retrieving Tables");
+		return admin.listTableNames();
 	}
 
 	public void exit() {
-		// CleanUp
-		System.exit(0);
+		try {
+			admin.getConnection().close();
+			System.exit(0);
+		} catch (IOException e) {
+			System.out.println("Error attempting to close connection");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -75,8 +88,9 @@ public class Main {
 
 	public static void initLogger() {
 		PatternLayout layout = new PatternLayout("%-5p %d %m%n");
-		ConsoleAppender appender = new ConsoleAppender();
+		FileAppender appender = new FileAppender();
 		appender.setName("PlayBall");
+		appender.setFile("playBall.log");
 		appender.activateOptions();
 		appender.setLayout(layout);
 		Logger.getRootLogger().addAppender(appender);
